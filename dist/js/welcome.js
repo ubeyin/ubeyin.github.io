c08c0a622609008d88033bfe118c8afc10445c41
Ubeyin 10 <77193278+ubeyin@users.noreply.github.com>
2021-12-06 15:51:46 +0600

    Add files via upload

--- dist/js/welcome.js
+++ dist/js/welcome.js
@@ -9,8 +9,8 @@ var i = setInterval(function () {
 
             if (a.includes("C200") === true) {
                 k = true;
-                document.querySelectorAll(".navbar button")[document.querySelectorAll(".navbar button").length - 2].style.display = 'inline-block';
-                document.querySelectorAll(".navbar button")[document.querySelectorAll(".navbar button").length - 3].style.display = 'inline-block';
+                document.querySelectorAll(".navbar button")[document.querySelectorAll(".navbar button").length - 2].style.display = '';
+                document.querySelectorAll(".navbar button")[document.querySelectorAll(".navbar button").length - 3].style.display = '';
             } else if (a.includes("C0") === true) {
                 k = true;
                 window.location.reload();
