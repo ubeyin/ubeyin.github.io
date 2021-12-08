9d2b41e89d0c053145079bf864295adc7e210b28
Ubeyin 10 <77193278+ubeyin@users.noreply.github.com>
2021-12-08 15:02:17 +0600

    Add files via upload

--- dist/js/home.js
+++ dist/js/home.js
@@ -47,43 +47,44 @@ function switch_product() {
     document.getElementById("it4").getElementsByClassName("_active")[0].classList.remove("none");
     setCookie("mtr8y457", "4");
 }
-
-document.getElementById("it1").onclick = function () {
-    switch_home();
-}
-document.getElementById("it2").onclick = function () {
-    switch_friend();
-}
-document.getElementById("it3").onclick = function () {
-    switch_watch();
-}
-document.getElementById("it4").onclick = function () {
-    switch_product();
-}
-
-document.getElementById("menubar").onclick = function () {
-    if (document.querySelectorAll(".dropdown")[0].className === "dropdown none") {
-        document.querySelectorAll(".dropdown")[0].classList.remove("none");
-        document.querySelectorAll("#menubar")[0].style.backgroundColor = "rgba(0,0,0,.06)";
-    } else if (document.querySelectorAll(".dropdown")[0].className === "dropdown") {
-        document.querySelectorAll(".dropdown")[0].classList.add("none");
-        document.querySelectorAll("#menubar")[0].style.backgroundColor = "transparent";
-    }
-}
-
-window.onload = function () {
-    if (getCookie('mtr8y457') === "1") {
+if (document.getElementById("it1")) {
+    document.getElementById("it1").onclick = function () {
         switch_home();
-    } else if (getCookie('mtr8y457') === "2") {
+    }
+    document.getElementById("it2").onclick = function () {
         switch_friend();
-    } else if (getCookie('mtr8y457') === "3") {
-        switch_watch();
-    } else if (getCookie('mtr8y457') === "4") {
-        switch_product();
-    } else {
-        switch_home();
     }
-};
+    document.getElementById("it3").onclick = function () {
+        switch_watch();
+    }
+    document.getElementById("it4").onclick = function () {
+        switch_product();
+    }
+
+    document.getElementById("menubar").onclick = function () {
+        if (document.querySelectorAll(".dropdown")[0].className === "dropdown none") {
+            document.querySelectorAll(".dropdown")[0].classList.remove("none");
+            document.querySelectorAll("#menubar")[0].style.backgroundColor = "rgba(0,0,0,.06)";
+        } else if (document.querySelectorAll(".dropdown")[0].className === "dropdown") {
+            document.querySelectorAll(".dropdown")[0].classList.add("none");
+            document.querySelectorAll("#menubar")[0].style.backgroundColor = "transparent";
+        }
+    }
+
+    window.onload = function () {
+        if (getCookie('mtr8y457') === "1") {
+            switch_home();
+        } else if (getCookie('mtr8y457') === "2") {
+            switch_friend();
+        } else if (getCookie('mtr8y457') === "3") {
+            switch_watch();
+        } else if (getCookie('mtr8y457') === "4") {
+            switch_product();
+        } else {
+            switch_home();
+        }
+    };
+}
 
 /**************/
 
@@ -95,7 +96,10 @@ var i = setInterval(function () {
 
             if (a.includes("C200") === true) {
                 k = true;
-                document.querySelectorAll(".home-lbox")[0].style.display = "none";
+                document.querySelectorAll(".home-lbox")[document.querySelectorAll(".home-lbox").length - 1].style.display = "none";
+                if (document.querySelectorAll(".home2")[0]) {
+                    document.querySelectorAll(".home2")[0].style.display = "";
+                }
                 document.querySelectorAll(".home")[0].style.display = "";
             } else if (a.includes("C0") === true) {
                 k = true;
